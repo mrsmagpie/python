ec890060e8b18a105d0a0697bcb5133952fd9a1b
Kate <katesabadash@MacBook-Air-Kate.local>
2018-08-03 21:19:04 +0300

    initial commit

---
+++ flashcards.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import sys
+import random
+
+if len(sys.argv) < 2:
+    print("Please add a flash card file.")
+    exit(1)
+
+flashcard_filename = sys.argv[1]
+f = open(flashcard_filename, "r")
+
+question_dict = {}
+
+for line in f:
+    entry = line.strip().split(",")
+    question = entry[0]
+    answer = entry[1]
+    question_dict[question] = answer
+
+f.close()
+
+print("Welcome to the flashcard quizzer.")
+print("At any time, type 'quit' to quit.")
+print("")
+
+questions = list(question_dict.keys())
+
+while True:
+    question = random.choice(questions)
+    answer = question_dict[question]
+    print("Question: " + question)
+    user_input = input("Your guess: ")
+    if user_input == "quit":
+        print("Thanks for playing! See you later.")
+        break
+    elif user_input.lower() == answer.lower():
+        print("You are awesome!")
+    else:
+        print("Sorry, the answer is: " + answer)
